649ec2d2b8df47ceb7d0ff2f021d19e3c0ba73e5
gbelleuvre <gbelleuvre@atecna.fr>
2016-05-13 14:52:08 +0200

    nettoyage du gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,10 @@ var	gulp = require('gulp'),
 	less = require('gulp-less'),
 	jshint = require('gulp-jshint'),
 	lazyMinify = require('gulp-lazy-minify'),
-	
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
 	uglify = require('gulp-uglify'),
-	
-	// modernizr = require('gulp-modernizr');
 	watch = require('gulp-watch');
-
-/** Les watch pour effectuer les taches à chaque modification de fichier ne peuvent pas fonctionner en distant.
-  *	Prévoir la mise en place des watch quand node.js pourra être installé sur le serveur.
-  */
-
 
 // Répertoires
 var app = './app',
@@ -38,19 +30,16 @@ var modules = [
 	
 	// MODULES EXTERNES
 	
-	// jQuery 1.11
+	// AngularJS
 	mods + '/angular/angular.min.js',
 	mods + '/angular-route/angular-route.min.js',
 	
 	// NOS MODULES
-	
 	app + '/js/altima.js',
 	app + '/js/altimaArticles.js',
 	app + '/js/altimaArticle.js',
 	app + '/js/altimaContact.js',
 	app + '/js/altimaCarousel.js',
-
-	// LANCEMENTS
 	
 	// Script centrale
 	app + '/js/main.js'
@@ -75,41 +64,6 @@ gulp.task('scripts', function() {
 	console.log("Regroupement des JS OK !");
 });
 
-/*
-// MODERNIZR A PART
-gulp.task('scripts:modernizr', function() {	
-	gulp.src(modules)
-        .pipe(concat('scripts.js'))
-		.pipe(modernizr())
-		.pipe(lazyMinify())
-        .pipe(gulp.dest(dist + '/js'));
-		
-	console.log("Modernizr OK !");
-});
-
-
-
-// CREATION DU JAVASCRIPT POUR VIEUX NAVIGATEUR (pour le footer)
-var modulesOld = [
-	
-	// MODULES EXTERNES
-	
-	// HTML5 Shiv
-	mods + '/html5shiv/dist/html5shiv.js',
-	// HTML5 Shiv
-	mods + '/respond.js/dest/respond.min.js',
-
-];
-
-gulp.task('scripts:old', function() {	
-	gulp.src(modulesOld)
-        .pipe(concat('scripts.old.js'))
-		.pipe(lazyMinify())
-        .pipe(gulp.dest(dist + '/js'));
-		
-	console.log("Regroupement des JS pour vieux navigateurs OK !");
-});
-*/
 
 gulp.task('default', function() {
 	
@@ -118,10 +72,6 @@ gulp.task('default', function() {
 	gulp.start('less');
 	
 	gulp.start('scripts');
-	
-	// gulp.start('scripts:old');
-	
-	// gulp.start('scripts:modernizr');
 
 	watch(app + '/less/**/*.less', function($event) {
 		gulp.start('less');
